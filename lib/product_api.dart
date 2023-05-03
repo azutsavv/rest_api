aa4b798547fc124820791cb8aa5f87b27faa8af5
Utsav Singh <azutsav2002@gmail.com>
2023-05-03 20:41:54 +0530

    get_api_complete

--- lib/product_api.dart
+++ lib/product_api.dart
@@ -34,15 +34,69 @@ class _ProductState extends State<Product> {
       ),
       body: Column(
         children: [
-          Expanded(child:
-          FutureBuilder(
-            future: getproduct(),
-            builder: (context,Snapshot) {
-            return ListView.builder(
-              itemCount: Snapshot.data!.data!.length,
-              itemBuilder: (context, Index) {
-              });
-          }))
+          Expanded(
+              child: FutureBuilder<ProductModel>(
+                  future: getproduct(),
+                  builder: (context, Snapshot) {
+                    if (!Snapshot.hasData) {
+                      return Container(
+                          alignment: Alignment.center,
+                          child: SizedBox(
+                            height: 40,
+                            child: CircularProgressIndicator(),
+                          ));
+                    } else {
+                      return ListView.builder(
+                          itemCount: Snapshot.data!.data!.length,
+                          itemBuilder: (context, Index) {
+                            return Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ListTile(
+                                  title: Text(Snapshot.data!.data![Index].shop!.name.toString()),
+                                  subtitle: Text(Snapshot.data!.data![Index].shop!.shopemail.toString()),
+                                  leading: CircleAvatar(
+                                    backgroundImage: NetworkImage(Snapshot.data!.data![Index].shop!.image.toString()),
+                                  ),
+                                ),
+                                Container(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.3,
+                                  width:
+                                      MediaQuery.of(context).size.width * 1,
+                                  child: ListView.builder(
+                                      scrollDirection: Axis.horizontal,
+                                      itemCount: Snapshot.data!.data![Index].images!.length,
+                                      itemBuilder: (context, position) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        
+                                       height:
+                                        MediaQuery.of(context).size.height * 0.25,
+                                                                      width:
+                                        MediaQuery.of(context).size.width * 0.5, 
+                                       
+                                       decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+
+
+                                      image: DecorationImage(
+                                        image: NetworkImage(Snapshot.data!.data![Index].images![position].url.toString()),
+                                        fit: BoxFit.cover
+                                        )
+                                        
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                                ),
+                              ],
+                            );
+                          });
+                    }
+                  }))
         ],
       ),
     );
